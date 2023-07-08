3497c22d64bc5174331b4eba3b117169109546f7
Dana Vasilchuk <danavasilchuk@gmail.com>
2023-07-07 20:23:57 -0700

    update all files

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Pig Latin Translator</title>
-</head>
-<body>
-  <h1>Pig Latin Translator</h1>
-
-  <script>
+document.addEventListener("DOMContentLoaded", function () {
     // Prompt the user for input
     const input = prompt("Enter a sentence or a word:");
-
+  
     // Check if the input is not empty or a single-letter word
     if (input.trim() === "" || input.trim().length === 1) {
       alert("Input can't be translated.");
     } else {
       // Split the input into individual words
       const words = input.split(" ");
-
+  
       // Array to store translated words
       const translatedWords = [];
-
+  
       // Iterate over each word and apply Pig Latin rules
       for (let word of words) {
         // Convert the word to lowercase for consistent comparison
         const lowerCaseWord = word.toLowerCase();
-
+  
         // Check if the word starts with a vowel
         if (lowerCaseWord[0].match(/[aeiou]/)) {
           translatedWords.push(word + "way");
@@ -39,13 +31,16 @@
           }
         }
       }
-
+  
       // Build the translated sentence
       const translatedSentence = translatedWords.join(" ");
-
-      // Display the translated sentence
-      alert("Translated sentence: " + translatedSentence);
+  
+      // Create a paragraph element to display the translated sentence
+      const translatedOutput = document.createElement("p");
+      translatedOutput.textContent = "Translated sentence: " + translatedSentence;
+  
+      // Append the translated output to the body of the page
+      document.body.appendChild(translatedOutput);
     }
-  </script>
-</body>
-</html>
+  });
+  
