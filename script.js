10f5a418c58ce3f2e224be376a334db3b032e6b4
Dana Vasilchuk <danavasilchuk@gmail.com>
2023-07-07 21:20:45 -0700

    update changes

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const converter = new showdown.Converter();
+  
     // Fetch the README.md content
     fetch('README.md')
       .then(response => response.text())
       .then(data => {
-        // Convert the markdown content to HTML
-        const converter = new showdown.Converter();
         const htmlContent = converter.makeHtml(data);
-  
-        // Get the placeholder div element
         const readmeDiv = document.getElementById('readme-content');
-  
-        // Set the converted HTML content as the innerHTML of the div
         readmeDiv.innerHTML = htmlContent;
-  
-        // Prompt the user for input
-        const input = prompt("Enter a sentence or a word:");
-  
-        // Rest of your existing code...
+      })
+      .catch(error => {
+        console.error('Error fetching README.md:', error);
       });
+  
+    // Prompt the user for input
+    const input = prompt("Enter a sentence or a word:");
+  
+    // Check if the input is not empty or a single-letter word
+    if (input.trim() === "" || input.trim().length === 1) {
+      alert("Input can't be translated.");
+    } else {
+      const words = input.split(" ");
+      const translatedWords = [];
+  
+      for (let word of words) {
+        const lowerCaseWord = word.toLowerCase();
+  
+        if (lowerCaseWord[0].match(/[aeiou]/)) {
+          translatedWords.push(word + "way");
+        } else if (lowerCaseWord[0].match(/[bcdfghjklmnpqrstvwxyz]/)) {
+          if (lowerCaseWord[1].match(/[bcdfghjklmnpqrstvwxyz]/)) {
+            translatedWords.push(word.slice(2) + word.slice(0, 2) + "ay");
+          } else {
+            translatedWords.push(word.slice(1) + word[0] + "ay");
+          }
+        }
+      }
+  
+      const translatedSentence = translatedWords.join(" ");
+      const translationResult = document.getElementById('translation-result');
+      translationResult.textContent = "Translated sentence: " + translatedSentence;
+    }
   });
   
